a044e8f4ff5d01c5fc23d1ea7f0197deb4ec8746
gregorybfoulkes <gregorybfoulkes@gmail.com>
2022-04-25 12:53:44 +0200

    savong progress

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,27 @@
-import { useState } from 'react'
+import React from 'react';
+import axios from 'axios';
+import { useState,useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [message, setMessage] = useState('');
   const [newFilter, setNewFilter] = useState('');
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -48,8 +55,6 @@ const App = () => {
       setNewName('')
       setNewNumber('')
       setMessage('')
-      console.log('button clicked', newName, newNumber)
-
     }
    
   }
--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const PersonForm = ({newName,newNumber,handleNameChange,handleNumberChange,addPerson}) => {
 
     return (
